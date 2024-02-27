25014d9de503670037fb24d14f8ca854207170c3
MINIFI2548 <Sarayuth2548@gmail.com>
2024-02-27 23:28:13 +0700

    Ver. 0.0.4

--- script.js
+++ script.js
@@ -5,44 +5,44 @@ let paper = document.querySelector(".paper");
 let gitbox = document.querySelector(".box");
 let cont = document.querySelector('.emoji-container');
 
-// // ! เปิดซองจดหมาย
+// ! เปิดซองจดหมาย
 
-// body.addEventListener('click', openEnvelope);
-// function openEnvelope(){
-//     console.log('open envelope');
-//     body.removeEventListener('click', openEnvelope);
+body.addEventListener('click', openEnvelope);
+function openEnvelope(){
+    console.log('open envelope');
+    body.removeEventListener('click', openEnvelope);
 
-//     envelope.classList.add('open');
-//     paperCont.classList.add('opening');
-//     paperCont.addEventListener('animationend', function(){
-//         paperCont.classList.remove('opening');
-//         paperCont.classList.add('opened');
-//         envelope.style.animation = 'spinout-envelope 1s .2s linear';
-//         envelope.addEventListener('animationend', function(){
-//             envelope.remove();
-//         },{once : true}
-//         );
-//     body.addEventListener('click', readLetter);
-//     },{once : true}
-//     );
-//     paperCont.classList.remove('in-envelope');
-//     console.log(envelope);
-// }
+    envelope.classList.add('open');
+    paperCont.classList.add('opening');
+    paperCont.addEventListener('animationend', function(){
+        paperCont.classList.remove('opening');
+        paperCont.classList.add('opened');
+        envelope.style.animation = 'spinout-envelope 1s .2s linear';
+        envelope.addEventListener('animationend', function(){
+            envelope.remove();
+        },{once : true}
+        );
+    body.addEventListener('click', readLetter);
+    },{once : true}
+    );
+    paperCont.classList.remove('in-envelope');
+    console.log(envelope);
+}
 
-// // ! เปิดการ๋ดทีละใบ
-// function readLetter(){
-//     console.log(paperCont);
-//     paperCont.classList.add('drop');
-//     paperCont.addEventListener('animationend', function(){
-//         gitbox.classList.add('on');
-//         body.removeEventListener('click', readLetter);
-//         paperCont.remove();
-//     },{once : true}
-//     );
-//     body.addEventListener('click', boxExplosion);
-// }
+// ! เปิดการ๋ดทีละใบ
+function readLetter(){
+    console.log(paperCont);
+    paperCont.classList.add('drop');
+    paperCont.addEventListener('animationend', function(){
+        gitbox.classList.add('on');
+        body.removeEventListener('click', readLetter);
+        paperCont.remove();
+    },{once : true}
+    );
+    body.addEventListener('click', boxExplosion);
+}
 
-body.addEventListener('click', boxExplosion);
+// body.addEventListener('click', boxExplosion);
 // ! เปิดกล่องของขวัญ
 function boxExplosion(){
     console.log("box boom");
